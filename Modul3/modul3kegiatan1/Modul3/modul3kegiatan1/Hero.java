8fa23423ad0b0b8f4003001c04dc69f45d221226
Minsa111 <dragonriderrgt@gmail.com>
2022-05-23 09:33:25 +0700

    ok sip

--- Modul3/modul3kegiatan1/Modul3/modul3kegiatan1/Hero.java
+++ Modul3/modul3kegiatan1/Modul3/modul3kegiatan1/Hero.java
@@ -3,7 +3,7 @@ package Modul3.modul3kegiatan1;
 
 
 class Hero {
-    String Name, Role, Skill, Weapon, Title = "\n========================\n===Hero===\n========================";
+    public String Name, Role, Skill, Weapon, Title = "\n========================\n===Hero===\n========================";
     public void NameofGame (){
         System.out.println("===Valorant's Heroes===");
     }
--- Modul3/modul3kegiatan2/Parentss/Main.java
+++ Modul3/modul3kegiatan2/Parentss/Main.java
@@ -4,7 +4,7 @@ import Childss.Child;
 
 public class Main {
     public static void main(String[] args) {
-        Child b = new Child();;
+        Child b = new Child();
         String Title ="Valorant";
         String AgentName = "Jett";
         String Ultimate = "Knives";
@@ -12,7 +12,6 @@ public class Main {
         b.setNum(20);
         new Child("Bind");
         b.IsaImplementation(AgentName, Ultimate, Title);
-        System.out.println("\n\nAcces modifier implementation");
         b.idk("Bind", "Jett", b.getNum());
     }
 }
